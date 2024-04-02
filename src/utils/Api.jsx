5412d596e876fd1711380a5ac5860b345a7360d2
Ritika Dwivedi <ritikadwivedi2701@gmail.com>
2024-04-02 20:44:27 +0530

    Updated

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,2 +1,3 @@
 // export const api = "https://project-management-app-4kjw.onrender.com";
-export const api = "http://localhost:2704";
+// export const api = "http://localhost:2704";
+export const api = "https://digishivar-backend.onrender.com";
